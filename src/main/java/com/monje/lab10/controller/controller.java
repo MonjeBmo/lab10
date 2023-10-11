fcb1d0a4d2dfc5589816557caef04c95291a7a46
Victor Monje <monjev086@gmail.com>
2023-10-11 10:00:06 -0600

    init commit

---
+++ src/main/java/com/monje/lab10/controller/controller.java
@@ -0,0 +1,29 @@
+package com.monje.lab10.controller;
+
+import com.monje.lab10.user.User;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class controller {
+
+    //function generate csv file
+    public void generateCSV(User user) {
+        try {
+            String path = "src/main/java/com/monje/lab10/data/user.csv";
+            File file = new File(path);
+            FileWriter fileWriter = new FileWriter(path, true);
+
+            if (file.length() == 0) {
+                fileWriter.write("Nombre, Apellido, Edad, Direccion, Genero\n");
+            }
+            fileWriter.write(user.getName() + "," + user.getLastName() + "," + user.getAge() + "," + user.getDirection() + "," + user.getGender() + "\n");
+
+            fileWriter.close();
+        } catch (Exception e) {
+            System.out.println("An error occurred." + e.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/com/monje/lab10/user/User.java
@@ -0,0 +1,49 @@
+package com.monje.lab10.user;
+
+public class User {
+    String name;
+    String lastName;
+    int age;
+    String direction;
+    String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
